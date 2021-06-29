e2c18704d66270d90c461c1182d4f20e81dd7a8d
chmwang <chmwang@zju.edu.cn>
2021-06-29 16:28:54 +0800

    check data hash when analysis

--- ipplus360/ip_search/enclave/ip_search_parser.h
+++ ipplus360/ip_search/enclave/ip_search_parser.h
@@ -4,15 +4,16 @@
 #include "stbox/scope_guard.h"
 #include "stbox/stx_common.h"
 #include "stbox/tsgx/ocall.h"
-//#include "stbox/tsgx/ocall.h"
 #ifdef EXAMPLE_FM_NORMAL
 #include "glog/logging.h"
 #else
 #include "stbox/tsgx/log.h"
 #endif
-//#include "common/limits.h"
+#include "common/limits.h"
 #include "user_type.h"
 #include <hpda/extractor/raw_data.h>
+#include <hpda/output/memory_output.h>
+#include <hpda/processor/query/filter.h>
 #include <string.h>
 
 class ip_search_parser {
@@ -24,10 +25,16 @@ public:
 
   inline stbox::bytes do_parse(const stbox::bytes &param) {
     LOG(INFO) << "do parse";
+    hpda::processor::internal::filter_impl<user_item_t> match(
+        m_source, [&](const user_item_t &v) { return false; });
+    hpda::output::internal::memory_output_impl<user_item_t> mo(&match);
+    mo.run();
     uint32_t resp_size = ypc::utc::max_item_size;
     char *resp;
-    ff::scope_guard _resp_desc([&]() { resp = new char[resp_size]; },
-                               [&]() { delete[] resp; });
+    ff::scope_guard _resp_desc([&]() {
+      resp = new char[resp_size]; },
+                               [&]() {
+      delete[] resp; });
     uint32_t ret = stbox::ocall_cast<uint32_t>(ocall_http_request)(
         (const char *)param.data(), param.size(), resp, resp_size);
     if (ret) {
