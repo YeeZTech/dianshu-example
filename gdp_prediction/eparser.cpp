01c3829268b45e37d7567e3510497ff72b8ed9c8
Wang Chenmin <chmwang@zju.edu.cn>
2022-09-05 13:51:37 +0800

    noinput_data_stream

--- gdp_prediction/eparser.cpp
+++ gdp_prediction/eparser.cpp
@@ -4,7 +4,7 @@
 #include "ypc/core_t/analyzer/algo_wrapper.h"
 #include "ypc/core_t/analyzer/macro.h"
 
-ypc::algo_wrapper<ypc::crypto::eth_sgx_crypto, ypc::sealed_data_stream,
+ypc::algo_wrapper<ypc::crypto::eth_sgx_crypto, ypc::noinput_data_stream,
                   gdp_prediction_parser,
                   ypc::onchain_result<ypc::crypto::eth_sgx_crypto>>
     pw;
