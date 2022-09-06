104dda41aac19d5ab6fc0aaafd66d09e23e7e4c8
熊松林 <xsl@xiongsonglindeMacBook-Pro.local>
2022-09-06 11:27:52 +0800

    gdp query by quarter

---
+++ gdp-quarter-query/gdp_quarter_query_parser.h
@@ -0,0 +1,48 @@
+
+#include "ypc/stbox/ebyte.h"
+#include "ypc/stbox/stx_common.h"
+#include "ypc/core_t/analyzer/data_source.h"
+#include "ypc/stbox/tsgx/log.h"
+#include "user_type.h"
+
+#include "ypc/corecommon/to_type.h"
+#include <hpda/extractor/raw_data.h>
+#include <hpda/output/memory_output.h>
+#include <hpda/processor/query/filter.h>
+
+typedef ff::net::ntpackage<0, province_name > nt_package_t;
+
+class gdp_quarter_query_parser {
+public:
+  gdp_quarter_query_parser(ypc::data_source<stbox::bytes> *source)
+      : m_source(source){};
+
+  inline stbox::bytes do_parse(const stbox::bytes &param) {
+    ypc::to_type<stbox::bytes, gdp_quarter_query_item_t> converter(m_source);
+
+    auto pkg = ypc::make_package<nt_package_t>::from_bytes(param);
+    hpda::processor::internal::filter_impl<gdp_quarter_query_item_t> match(
+        &converter, [&](const gdp_quarter_query_item_t &v) {
+          std::string first_item = v.get<province_name>();
+          if ( first_item == pkg.get<province_name>() ) {
+            return true;
+          }
+          return false;
+        });
+
+    hpda::output::internal::memory_output_impl<gdp_quarter_query_item_t> mo(&match);
+    mo.get_engine()->run();
+    LOG(INFO) << "do parse done";
+
+    stbox::bytes result("not found\n");
+    for (auto it : mo.values()) {
+      result = stbox::bytes(it.get< gdp_quarters >());
+      break;
+    }
+    return result;
+  }
+
+protected:
+  ypc::data_source<stbox::bytes> *m_source;
+};
+
