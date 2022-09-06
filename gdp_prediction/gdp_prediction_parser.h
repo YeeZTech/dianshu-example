07d85a726a82b68616cec98efc5e2889f5434982
chmwang <chmwang@zju.edu.cn>
2022-09-06 10:51:33 +0800

    revert no_input_data_stream, for checking data hash

--- gdp_prediction/gdp_prediction_parser.h
+++ gdp_prediction/gdp_prediction_parser.h
@@ -14,8 +14,25 @@ typedef ff::net::ntpackage<0, radiance_sum> nt_package_t;
 
 class gdp_prediction_parser {
 public:
+  gdp_prediction_parser(ypc::data_source<stbox::bytes> *source)
+      : m_source(source){};
+
   inline stbox::bytes do_parse(const stbox::bytes &param) {
+    ypc::to_type<stbox::bytes, gdp_prediction_item_t> converter(m_source);
+
     auto pkg = ypc::make_package<nt_package_t>::from_bytes(param);
+
+    hpda::processor::internal::filter_impl<gdp_prediction_item_t> match(
+        &converter, [&](const gdp_prediction_item_t &v) {
+          return true;
+        });
+
+    hpda::output::internal::memory_output_impl<gdp_prediction_item_t> mo(&match);
+
+    mo.get_engine()->run();
+
+    LOG(INFO) << "do parse done";
+
     double slope = 4787.502598784844;
     double vertical_intercept = -80.42706901157614;
     auto rad = pkg.get<radiance_sum>();
@@ -23,8 +40,10 @@ public:
     auto res = slope * radiance + vertical_intercept;
     auto temp = std::to_string( res );
     stbox::bytes result( temp );
-    LOG(INFO) << "do parse done";
     return result;
   }
+
+protected:
+  ypc::data_source<stbox::bytes> *m_source;
 };
 
