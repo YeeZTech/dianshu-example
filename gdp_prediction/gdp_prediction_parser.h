e23984beae7a5c0c53c1bd823a67fbc3e20ed08c
褴褛行仙 <10749201+ragged-immortal@user.noreply.gitee.com>
2022-09-27 16:14:04 +0800

    *

--- gdp_prediction/gdp_prediction_parser.h
+++ gdp_prediction/gdp_prediction_parser.h
@@ -26,7 +26,11 @@ public:
     hpda::processor::internal::filter_impl<gdp_prediction_item_t> match(
         &converter, [&](const gdp_prediction_item_t &v) {
           std::string first_item = v.get<name>();
-          if ( first_item == pkg.get< name >() && pkg.get< name >().find( "省" ) == std::string::npos && pkg.get< name >().find( "自治区" ) == std::string::npos && pkg.get< name >().find( "特别行政区" ) == std::string::npos && pkg.get< name >().find( "国" ) == std::string::npos ) {
+          // LOG(INFO) << "first: " << first_item;
+          // LOG(INFO) << "input: " << v.get<name>();
+          if ( first_item == pkg.get< name >() ) {
+            LOG(INFO) << "-->first: " << first_item;
+            LOG(INFO) << "-->input: " << v.get<name>();
             return true;
           }
           return false;
@@ -36,29 +40,38 @@ public:
     LOG(INFO) << "into run";
     mo.get_engine()->run();
     LOG(INFO) << "do parse done";
-    double year_rad = 0;
+    double year_rad = 0.0;
     double ar = 0;
     bool is_found = false;
+    bool is_month_covered[ 12 ];
+    for ( int i = 0; i < 12; ++ i ) {
+      is_month_covered[ i ] = false;
+    }
     for (auto it : mo.values()) {
       is_found = true;
       int ym = std::stoi( it.get< year_month >() );
       ar = std::stod( it.get< area >() );
-      double rp = std::stod( it.get< rad_perarea >() );
+      double rp = std::stod( it.get< rad >() );
       if ( ym < 202113 && 202100 < ym ) {
-        year_rad += rp;
+        if ( !is_month_covered[ ym % 100 - 1 ] ) {
+          year_rad += rp;
+          LOG(INFO) << "-----: " << ym % 100 << " year_rad " << year_rad;
+          LOG(INFO) << "month: " << ym % 100 << " monthrad " << rp;
+          is_month_covered[ ym % 100 - 1 ] = true;
+        }
       }
     }
 
-    double slope = 4787.502598784844;
-    double vertical_intercept = -80.42706901157614;
-    auto res = slope * ( year_rad / 12 ) + vertical_intercept;
-    auto temp = std::to_string( res * ar );
+    double slope = 477.64200343479285;
+    double vertical_intercept = -550.769860260704;
+    auto res = slope * ( year_rad / 12.0 ) + vertical_intercept * ar;
+    auto temp = std::to_string( res );
     stbox::bytes result( "地区名称 ( 区县 ) , 2021年GDP预估 ( 万元 ) \n" );
     result += stbox::bytes( pkg.get< name >() );
     result += stbox::bytes( "," );
     result += stbox::bytes( temp );
     result += stbox::bytes( "\n" );
-    if ( !is_found || res * ar < 10000 )
+    if ( !is_found )
       result = stbox::bytes( "您输入的参数不能匹配到对应的地区, 请重新提交\n" );
     return result;
   }
