1beb08429b3b567565ddb374c1a11e2167a2e3ac
褴褛行仙 <10749201+ragged-immortal@user.noreply.gitee.com>
2022-09-27 10:25:31 +0800

    *

--- radiance-query/radiance_query_parser.h
+++ radiance-query/radiance_query_parser.h
@@ -57,7 +57,7 @@ public:
           if ( a[ 1 ] == b[ 1 ] )
             return a[ 0 ] > b[ 0 ];
           else
-            return a[ 0 ] > b[ 0 ];
+            return a[ 1 ] > b[ 1 ];
 		    } );
     for (auto it : temp ) {
       flag = true;
