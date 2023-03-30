51d93ce79a2fdabaafc451cd2e016ec6ecc641ce
chwmang <chmwang@zju.edu.cn>
2023-03-30 17:34:40 +0800

    rm no use files

--- example/clickhouse/query/enclave/user_type.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <ff/util/ntobject.h>
-#include <string>
-define_nt(msisdn, std::string);
-define_nt(msisdn_hash, std::string);
-typedef ff::util::ntobject<msisdn, msisdn_hash> msisdn_item_t;
