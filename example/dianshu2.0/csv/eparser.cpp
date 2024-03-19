cb744ed69e7e76cad5bb262cc3ca8cc1acb7ca9b
chwmang <chmwang@zju.edu.cn>
2024-03-19 19:51:18 +0800

    make txt and csv offchain result

--- example/dianshu2.0/csv/eparser.cpp
+++ example/dianshu2.0/csv/eparser.cpp
@@ -6,7 +6,7 @@
 
 ypc::algo_wrapper<ypc::crypto::eth_sgx_crypto, ypc::multi_data_stream,
                   csv_evaluate_parser,
-                  ypc::onchain_result<ypc::crypto::eth_sgx_crypto>>
+                  ypc::offchain_result<ypc::crypto::eth_sgx_crypto>>
     pw;
 
 YPC_PARSER_IMPL(pw);
