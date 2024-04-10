0d01ad4536eb68006eeea268f5e0b8a4797f093e
JNWang17 <163986795+JNWang17@users.noreply.github.com>
2024-04-10 15:06:02 +0800

    Update txt_evaluate_parser_with_zh.h

--- src/dianshu2.0/txt/txt_evaluate_parser_with_zh.h
+++ src/dianshu2.0/txt/txt_evaluate_parser_with_zh.h
@@ -30,22 +30,37 @@ public:
     hpda::output::internal::memory_output_impl<txt_batch_item_t> mo(&match);
     mo.get_engine()->run();
 
+    // 将所有txt片段合并为一个字符串
     std::string s;
     for (auto &it : mo.values()) {
       s += it.get<::txt_batch>();
     }
     LOG(INFO) << "utf8 len: " << s.size();
+
+    // 将utf8字符串转换为u32string，以便处理中文字符
     auto u32s = to_u32string((uint8_t *)s.c_str(), s.size());
 
     json::JSON res;
-    res["total"] = std::to_string(u32s.size());
-    handle_txt_evaluate(u32s, res);
+
+    auto u32s_non_sc = std::u32string();
+
+    // 去除u32s中无法打印的特殊符号
+    for (size_t i = 0; i < u32s.size(); i++) {
+      if (u32s[i] >= 0x20) {
+        u32s_non_sc += u32s[i];
+      }
+    }
+
+    // 计算去除特殊字符后的总字符数
+    res["total"] = std::to_string(u32s_non_sc.size());
+    handle_txt_evaluate(u32s_non_sc, res);
     return stbox::bytes(res.dump());
   }
 
 protected:
   std::string from_u32string(char32_t c) {
     std::string s;
+    // 针对txt中可能出现的汉字进行额外处理
     if (c <= 0x7F) {
       s += static_cast<char>(c & 0x7F);
     } else if (c <= 0x7FF) {
@@ -71,6 +86,8 @@ protected:
     }
     return s;
   }
+
+  // 对文本进行评估的函数，提取头部、中部、尾部的文本，并添加到JSON结果中
   void handle_txt_evaluate(const std::u32string &u32s, json::JSON &res) {
     size_t words_cnt = u32s.size();
     size_t actual_words_cnt = std::min(size_t(150), size_t(0.1 * words_cnt));
